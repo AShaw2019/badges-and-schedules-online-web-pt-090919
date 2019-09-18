a4dbbfe2418990785145dcbb868494d681c3f490
Ashley Twomey <aprier091012@gmail.com>
2019-09-18 15:31:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+
+
+def batch_badge_creator
+  badge_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badge_names.each do |name|
+  puts "Hello, my name is #{name}."
+end
+end
